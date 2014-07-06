a42da68b67d7b9826e14452c2d91014974f2b899
xia0pin9 <lyp20062392@gmail.com>
2014-07-06 15:19:00 -0500

    First commit

---
+++ malcluster-runner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+Convenience wrapper for testing development version of bootstrap.
+"""
+ 
+from malcluster.main import main
+ 
+if __name__ == '__main__':
+    main()
